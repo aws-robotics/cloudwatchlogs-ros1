074cff069a5c4fc4066ec43f6db9d51316de637e
Miaofei <miaofei@amazon.com>
2019-05-22 23:00:10 -0700

    modify cloudwatch_logger to use add_rostest_gmock()
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- cloudwatch_logger/test/log_node_param_helper_test.cpp
+++ cloudwatch_logger/test/log_node_param_helper_test.cpp
@@ -34,7 +34,7 @@ using Aws::AwsError;
 class ParameterReaderMock : public ParameterReaderInterface 
 {
 public:
- MOCK_CONST_METHOD2(ReadParam, Aws::AwsError(const ParameterPath &, std::vector<std::string> &));
+  MOCK_CONST_METHOD2(ReadParam, Aws::AwsError(const ParameterPath &, std::vector<std::string> &));
   MOCK_CONST_METHOD2(ReadParam, Aws::AwsError(const ParameterPath &, double &));
   MOCK_CONST_METHOD2(ReadParam, Aws::AwsError(const ParameterPath &, int &));
   MOCK_CONST_METHOD2(ReadParam, Aws::AwsError(const ParameterPath &, bool &));
